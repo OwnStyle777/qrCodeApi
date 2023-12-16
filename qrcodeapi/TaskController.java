064b7e0c53fa6a08b26d44d223af8527d10865a6
OwnStyle777 <OwnStyleXD@gmail.com>
2023-12-16 11:21:10 +0100

    add method create image and refactoring

--- qrcodeapi/TaskController.java
+++ qrcodeapi/TaskController.java
@@ -1,5 +1,10 @@
 package qrcodeapi;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.apache.catalina.LifecycleState;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
@@ -9,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -20,41 +24,42 @@ public class TaskController {
 
 
     @GetMapping("/api/health")
-        public ResponseEntity healthStatus() {
-          return ResponseEntity.status(200).build();
+    public ResponseEntity healthStatus() {
+        return ResponseEntity.status(200).build();
 
-        }
-
-//    @GetMapping("/api/qrcode")
-//    public ResponseEntity qrcodeStatus() {
-//        return ResponseEntity.status(501).build();
-//
-//    }
+    }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<?> getImage(int size, String type) {
+    public ResponseEntity<?> getImage(String contents, int size, String type) {
+
+        if (contents.isBlank()) {
+
+            String errorMessage = "Contents cannot be null or blank";
+            return ResponseEntity
+                    .badRequest()
+                    .body("{\"error\": \"" + errorMessage + "\"}");
+        }
 
         if (size <= 350 && size >= 150) {
 
-            BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-            image.createGraphics().fillRect(0, 0, size, size);
+            BufferedImage bufferedImage = createImage(contents, size);
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            if(type.equals("png") || type.equals("gif") || type.equals("jpeg")){
-            try {
-                ImageIO.write(image, type, outputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            if (type.equals("png") || type.equals("gif") || type.equals("jpeg")) {
+                try {
+                    ImageIO.write(bufferedImage, type, outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            ByteArrayResource resource = new ByteArrayResource(outputStream.toByteArray());
+                ByteArrayResource resource = new ByteArrayResource(outputStream.toByteArray());
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.parseMediaType("image/" + type.toLowerCase()));
-            headers.setContentLength(resource.contentLength());
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.parseMediaType("image/" + type.toLowerCase()));
+                headers.setContentLength(resource.contentLength());
 
-            return new ResponseEntity<>(resource, headers, HttpStatus.OK);}
-            else{
+                return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+            } else {
                 String errorMessage = "Only png, jpeg and gif image types are supported";
                 return ResponseEntity
                         .badRequest()
@@ -65,5 +70,19 @@ public class TaskController {
         return ResponseEntity
                 .badRequest()
                 .body("{\"error\": \"" + errorMessage + "\"}");
+    }
+
+    public BufferedImage createImage(String contents, int size) {
+        QRCodeWriter writer = new QRCodeWriter();
+        BufferedImage bufferedImage = null;
+
+        try {
+            BitMatrix bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size);
+            bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        return bufferedImage;
     }
 }
